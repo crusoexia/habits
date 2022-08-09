2db5d35a55290963724fe998fe12d4dac819c566
crusoexia <xiawenlan1984@gmail.com>
2022-08-09 15:37:22 +0800

    relocate iterm2 configs

--- setup.mac.sh
+++ setup.mac.sh
@@ -50,10 +50,6 @@ ln -s "$HOME/.vim" nvim
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
-# iterm2
-cd ~
-ln -s "$BASEDIR/configs/com.googlecode.iterm2.plist" com.googlecode.iterm2.plist
-
 # oh-my-zsh
 cd ~
 brew install autojump
@@ -61,8 +57,10 @@ ln -s "$BASEDIR/configs/.zshrc" .zshrc
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # inform manual actions
+echo ""
 echo "Actions need manual execute:"
 echo "* enable oh-my-zsh plugins"
 echo "* install tmux plugin(prefix + I)"
 echo "* install vim plugins"
 echo "* install Meslo font via https://github.com/andreberg/Meslo-Font/tags"
+echo "sync iterm config with configs/com.googlecode.iterm2.plist"
