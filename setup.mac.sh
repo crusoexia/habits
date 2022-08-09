92d6290ff66d563f2979de2757330df40bc9771c
crusoexia <xiawenlan1984@gmail.com>
2022-08-09 11:38:09 +0800

    fix setup.mac.sh

--- setup.mac.sh
+++ setup.mac.sh
@@ -1,20 +1,18 @@
-#!/bin/bash
+#!/bin/zsh
 set -e
 
 # check preconditions
 git --version
+zsh --version
 
 # quick access
 BASEDIR=$(cd "$(dirname "$0")"; pwd -P)
 
 # homebrew
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-
-# oh-my-zsh
-cd ~
-brew install zsh
-chsh -s $(which zsh)
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/cxia/.profile
+echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/cxia/.zprofile
+eval "$(/opt/homebrew/bin/brew shellenv)"
 
 # git
 cd ~
@@ -23,7 +21,7 @@ ln -s "$BASEDIR/configs/.gitconfig" .gitconfig
 # tmux
 cd ~
 brew install tmux
-ln -s "$BASEDIR/configs/.tmux.config" .tmux.config
+ln -s "$BASEDIR/configs/.tmux.conf" .tmux.conf
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
 # nodejs
@@ -42,17 +40,27 @@ ln -s "$BASEDIR/configs/.fzf.config.zsh" .fzf.config.zsh
 # neovim
 cd ~
 brew install neovim
+python3 -m pip install --user --upgrade pynvim # enable python plugins
 mkdir .vim
 cd .vim
 ln -s "$BASEDIR/configs/.vim_init.vim" init.vim
 ln -s "$BASEDIR/configs/.vim_coc-settings.json" coc-settings.json
+cd ~/.config
+ln -s "$HOME/.vim" nvim
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
 # iterm2
 cd ~
 ln -s "$BASEDIR/configs/com.googlecode.iterm2.plist" com.googlecode.iterm2.plist
+
+# oh-my-zsh
+cd ~
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # inform manual actions
 echo "Actions need manual execute:"
 echo "* enable oh-my-zsh plugins"
 echo "* install tmux plugin(prefix + I)"
 echo "* install vim plugins"
+echo "* install Meslo font via https://github.com/andreberg/Meslo-Font/tags"
