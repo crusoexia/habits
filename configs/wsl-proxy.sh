2351b7d13200bd19be7af6a264ddcfb5508fa8e5
Crusoe Xia <xiawenlan1984@gmail.com>
2024-09-04 08:00:12 +0800

    fix wsl proxy setting

--- configs/wsl-proxy.sh
+++ configs/wsl-proxy.sh
@@ -1,7 +1,7 @@
 # Note: make sure the windows firewall had been configured to allow http connection
 # between wsl and windows
 
-host_ip=$(cat /etc/resolv.conf | grep nameserver | grep -oE '[0123456789.]*')
+host_ip=$(ip route | grep default | grep -oE '([0123456789.]+){4}')
 export ALL_PROXY="socks://$host_ip:10810"
 export http_proxy="http://$host_ip:10811"
 export https_proxy="http://$host_ip:10811"
