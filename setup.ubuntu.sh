eaa9d6c2e7b439d8b5ca123ec793c05dd8cf7f0b
crusoexia <xiawenlan1984@gmail.com>
2025-02-03 17:50:22 +0800

    fix: setup.ubuntu.sh
    separate .gitconfig for work and personal

--- setup.ubuntu.sh
+++ setup.ubuntu.sh
@@ -3,7 +3,6 @@
 set -e
 
 # vars
-declare HABITS_REPO=git@github.com:crusoexia/habits.git
 declare HABITS_HOME=~/habits
 
 # update apt
@@ -22,14 +21,18 @@ zsh --version
 # git global config
 cd ~
 rm -f .gitconfig
-ln -s "$HABITS_HOME/configs/.gitconfig" .gitconfig
+ln -s "$HABITS_HOME/configs/.gitconfig-personal" .gitconfig
 
 # tmux
 cd ~
 sudo apt install -y tmux
 sudo apt install -y tmuxinator
+
+rm -f .tmux.conf
 ln -s "$HABITS_HOME/configs/.tmux.conf" .tmux.conf
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+rm -rf .tmux
+git clone https://github.com/tmux-plugins/tpm.git ~/.tmux/plugins/tpm
 
 # nodejs
 cd ~
@@ -43,6 +46,8 @@ sudo apt install -y ripgrep
 sudo apt install -y fd-find
 source ~/.bashrc
 mkdir -p ~/.local/bin
+
+rm -f ~/.local/bin/fd
 ln -s $(which fdfind) ~/.local/bin/fd
 
 # python3
@@ -50,23 +55,26 @@ sudo apt install -y python3-pip
 source ~/.bashrc
 
 # neovim
+# Note: 
+# Below pre-built destro url might change as time pass-by.
+# Check https://github.com/neovim/neovim/blob/master/INSTALL.md to double confirm the correct path.
 cd ~
-curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz
 sudo rm -rf /opt/nvim
-sudo tar -C /opt -xzf nvim-linux64.tar.gz
-sudo ln -s /opt/nvim-linux64/bin/nvim  /usr/bin/nvim
-rm -rf nvim-linux64.tar.gz
+sudo tar -C /opt -xzf nvim-linux-x86_64.tar.gz
+sudo ln -sf /opt/nvim-linux-x86_64/bin/nvim  /usr/bin/nvim
+rm -rf nvim-linux-x86_64.tar.gz
 
 sudo apt install -y python3-neovim
-python3 -m pip install --user --upgrade pynvim # enable python plugins
+sudo apt install -y python3-pynvim # enable python plugins
 
 rm -rf .vim
 mkdir .vim
 cd .vim
-ln -s "$HABITS_HOME/configs/.vim_init.vim" init.vim
-ln -s "$HABITS_HOME/configs/.vim_coc-settings.json" coc-settings.json
+ln -sf "$HABITS_HOME/configs/.vim_init.vim" init.vim
+ln -sf "$HABITS_HOME/configs/.vim_coc-settings.json" coc-settings.json
 mkdir ~/.config && cd ~/.config
-ln -s "$HOME/.vim" nvim
+ln -sf "$HOME/.vim" nvim
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
@@ -102,7 +110,7 @@ echo "Actions need manual execute:"
 echo "* install nodejs"
 echo "* install tmux plugin(prefix + I)"
 echo "* install vim plugins"
-echo "* ln -s ~/onedrive/config/tmuxinator .config/"
+echo "* ln -s ~/OneDrive/config/tmuxinator .config/"
 echo "*********************************"
 echo ""
 echo " * Happy hacking! * "
